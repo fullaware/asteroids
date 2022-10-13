80b48c95484eb6d4ac7c6d7e752f6b9cb86a47f2
Full Aware <fullaware@users.noreply.github.com>
2022-10-12 22:44:16 -0400

    mysql dockerfile failure

--- dbinit/asteroids_load.sh
+++ dbinit/asteroids_load.sh
@@ -2,7 +2,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install mysql-server -y
-mysqladmin -u root password 'IamR00t'
-mysql -u root -pIamr00t -e "CREATE USER 'asteroid'@'%' IDENTIFIED BY 'IamR00t'; GRANT ALL PRIVILEGES ON *.* TO 'asteroid'@'%' WITH GRANT OPTION;"
-mysql -u reader -pIamr00t -e "CREATE DATABASE asteroids DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
-mysql -u reader -pIamr00t < asteroids.sql
+mysqladmin -u root password 'I@mR00t'
+mysql -u root -pI@mR00t -e "CREATE USER 'asteroid'@'%' IDENTIFIED BY 'IamR00t'; GRANT ALL PRIVILEGES ON *.* TO 'asteroid'@'%' WITH GRANT OPTION;"
+mysql -u asteroid -pI@mR00t -e "CREATE DATABASE asteroids DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+mysql -u asteroid -pI@mR00t < asteroids.sql
